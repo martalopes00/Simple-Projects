56a2a478685441ef1d064813eee5f5e89313951f
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-05 23:33:19 +0100

    Update is.py

--- is.py
+++ is.py
@@ -1,8 +1,23 @@
-algo=input('Escreva algo')
-print('O tipo primitivo desse valor é',type(algo))
-print('Só tem espaços?',algo.isspace())
-print('É um número?',algo.isnumeric())
-print('É alfabetico?',algo.isalpha())
-print('Está em maiúsculas?',algo.isupper())
-print('Está em minúsculas?',algo.islower())
-print('Está capitalizada?',algo.istitle())
+# Ask the user to type something
+something = input('Type something: ')
+
+# Show the primitive type of the input
+print('The primitive type of this value is', type(something))
+
+# Check if it contains only spaces
+print('Only spaces?', something.isspace())
+
+# Check if it is numeric
+print('Is it a number?', something.isnumeric())
+
+# Check if it is alphabetic
+print('Is it alphabetic?', something.isalpha())
+
+# Check if it is uppercase
+print('Is it uppercase?', something.isupper())
+
+# Check if it is lowercase
+print('Is it lowercase?', something.islower())
+
+# Check if it is capitalized (title case)
+print('Is it capitalized?', something.istitle())
