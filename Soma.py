76184f5bc97819db6f8906799de57197e83ffafa
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-05 02:57:16 +0100

    Update Soma.py

--- Soma.py
+++ Soma.py
@@ -1,4 +1,6 @@
 n1=int(input('Digite um valor: '))
 n2=int(input('Digite outro valor: '))
+#Soma os dois valores
 soma=n1+n2
+#Diz quanto é a soma entre os dois valores
 print('A soma entre {} e {} é igual a {}'.format(n1,n2,soma))
