ffc2bf83b8ac43e6020f17a05fed75f51f8bb1fe
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:05:40 +0100

    Update Conversão de medidas.py

--- Conversão de medidas.py
+++ Conversão de medidas.py
@@ -1,4 +1,11 @@
+# Pedir ao utilizador um valor em metros
 m = int(input('Escreva um valor em metros: '))
+
+# Converter metros para centímetros
 cm = m * 100
+
+# Converter metros para milímetros
 mm = m * 1000
-print('{} metros são {} cm e {} mm'.format(m,cm,mm))
+
+# Mostrar o resultado das conversões
+print('{} metros são {} cm e {} mm'.format(m, cm, mm))
