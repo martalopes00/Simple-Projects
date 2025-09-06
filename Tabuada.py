b1e8602ae6a711d6ee8aafde0c6ac469e8a02d3c
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:13:20 +0100

    Update Tabuada.py

--- Tabuada.py
+++ Tabuada.py
@@ -1,12 +1,17 @@
-n = int(input('Escreva um número: '))
+# Pedir ao utilizador para inserir um número
+n = int(input('Escreve um número: '))
+
+# Mostrar a mensagem da tabuada
 print('A tabuada de {} é:'.format(n))
-print('{}x1={}'.format(n,n*1))
-print('{}x2={}'.format(n,n*2))
-print('{}x3={}'.format(n,n*3))
-print('{}x4={}'.format(n,n*4))
-print('{}x5={}'.format(n,n*5))
-print('{}x6={}'.format(n,n*6))
-print('{}x7={}'.format(n,n*7))
-print('{}x8={}'.format(n,n*8))
-print('{}x9={}'.format(n,n*9))
-print('{}x10={}'.format(n,n*10))
+
+# Mostrar a tabuada de 1 a 10
+print('{}x1={}'.format(n, n*1))
+print('{}x2={}'.format(n, n*2))
+print('{}x3={}'.format(n, n*3))
+print('{}x4={}'.format(n, n*4))
+print('{}x5={}'.format(n, n*5))
+print('{}x6={}'.format(n, n*6))
+print('{}x7={}'.format(n, n*7))
+print('{}x8={}'.format(n, n*8))
+print('{}x9={}'.format(n, n*9))
+print('{}x10={}'.format(n, n*10))
