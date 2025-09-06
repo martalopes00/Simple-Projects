948d9919acb3ab9f7e3c9bee6a735de82a186ddf
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:09:18 +0100

    Update Dobro, triplo e raíz quadrada.py

--- Dobro, triplo e raíz quadrada.py
+++ Dobro, triplo e raíz quadrada.py
@@ -1,4 +1,11 @@
-n = int(input('Escreva um número: '))
-print('O dobro de {} é {}'.format(n,n*2))
-print('O triplo de {} é {}'.format(n,n*3))
-print('A raíz quadrada de {} é {}'.format(n,n**(1/2)))
+# Pedir ao utilizador para inserir um número
+n = int(input('Escreve um número: '))
+
+# Mostrar o dobro do número
+print('O dobro de {} é {}'.format(n, n*2))
+
+# Mostrar o triplo do número
+print('O triplo de {} é {}'.format(n, n*3))
+
+# Mostrar a raiz quadrada do número
+print('A raiz quadrada de {} é {}'.format(n, n**(1/2)))
