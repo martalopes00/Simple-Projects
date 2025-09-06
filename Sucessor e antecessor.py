5bd7a7d6252ce26c847e406775ac1e4f75a55a34
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:12:01 +0100

    Update Sucessor e antecessor.py

--- Sucessor e antecessor.py
+++ Sucessor e antecessor.py
@@ -1,2 +1,5 @@
-n = int(input('Escreva um número: '))
-print('O sucessor de {} é {} e o antecessor é {}' .format(n,n+1,n-1))
+# Pedir ao utilizador para inserir um número
+n = int(input('Escreve um número: '))
+
+# Mostrar o sucessor e o antecessor do número
+print('O sucessor de {} é {} e o antecessor é {}'.format(n, n+1, n-1))
