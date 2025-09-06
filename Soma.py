f585fe89b4127afc036695b8bf361301b5e0e939
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:12:43 +0100

    Update and rename Sum.py to Soma.py

---
+++ Soma.py
@@ -0,0 +1,13 @@
+# Pedir ao utilizador para inserir um número
+n1 = int(input('Escreve um número: '))
+
+# Pedir ao utilizador para inserir outro número
+n2 = int(input('Escreve outro número: '))
+
+# Somar os dois valores
+soma = n1 + n2
+
+# Mostrar a soma dos dois valores
+print('A soma de {} e {} é {}'.format(n1, n2, soma))
+
+
