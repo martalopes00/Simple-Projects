d2cdb655e6bebbd518f7d6ee45c8af41eff8a372
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:11:41 +0100

    Update Quantos litros de tinta preciso.py

--- Quantos litros de tinta preciso.py
+++ Quantos litros de tinta preciso.py
@@ -1,3 +1,9 @@
-Largura = float(input('Qual é a largura da sua parede em metros?: '))
-Altura = float(input('Qual é a altura da sua parede em metros?: '))
-print('A área da parede é {}m2 e são precisos {} litros de tinta para a pintar.'.format(Largura*Altura,(Largura*Altura)/2))
+# Pedir ao utilizador a largura da parede
+largura = float(input('Qual é a largura da tua parede em metros?: '))
+
+# Pedir ao utilizador a altura da parede
+altura = float(input('Qual é a altura da tua parede em metros?: '))
+
+# Calcular a área da parede e a quantidade de tinta necessária (1 litro para cada 2 m²)
+print('A área da parede é {}m2 e são precisos {} litros de tinta para a pintar.'.format(largura * altura, (largura * altura) / 2))
+
