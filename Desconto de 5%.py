9b3981ef241322e4a2af8cbca9444ceb8241c940
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:08:52 +0100

    Update Desconto de 5%.py

--- Desconto de 5%.py
+++ Desconto de 5%.py
@@ -1,2 +1,5 @@
-preço = float(input('Qual é o preço do produto?: '))
-print('Com desconto de 5% o produto vale {}'.format(preço-(preço*0.05)))
+# Pedir ao utilizador o preço do produto
+preco = float(input('Qual é o preço do produto?: '))
+
+# Calcular o preço com desconto de 5% e mostrar o resultado
+print('Com desconto de 5% o produto vale {}'.format(preco - (preco * 0.05)))
