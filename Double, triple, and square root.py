0573248df7f6e783e79c0ed6a873542d247493ce
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:26:24 +0100

    Update and rename Dobro, triplo e raíz quadrada.py to Double, triple, and square root.py

---
+++ Double, triple, and square root.py
@@ -0,0 +1,11 @@
+# Ask the user to enter a number
+n = int(input('Enter a number: '))
+
+# Show the double of the number
+print('The double of {} is {}'.format(n, n*2))
+
+# Show the triple of the number
+print('The triple of {} is {}'.format(n, n*3))
+
+# Show the square root of the number
+print('The square root of {} is {}'.format(n, n**(1/2)))
