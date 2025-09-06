24caabab7d40dcb239ed9d9637bb63a8870552bf
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:25:40 +0100

    Update and rename Conversão de medidas.py to Unit conversion.py

---
+++ Unit conversion.py
@@ -0,0 +1,11 @@
+# Ask the user to enter a value in meters
+m = int(input('Enter a value in meters: '))
+
+# Convert meters to centimeters
+cm = m * 100
+
+# Convert meters to millimeters
+mm = m * 1000
+
+# Show the result of the conversions
+print(f'{m} meters are {cm} cm and {mm} mm')
