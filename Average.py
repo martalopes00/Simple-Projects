7313b8bd7619dded673d87499565913bfee56278
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:27:06 +0100

    Update and rename Média.py to Average.py

---
+++ Average.py
@@ -0,0 +1,9 @@
+# Ask the user to enter two grades
+grade1 = float(input('Enter a grade: '))
+grade2 = float(input('Enter another grade: '))
+
+# Calculate the average of the two grades
+average = (grade1 + grade2) / 2
+
+# Show the average
+print('The average of the two grades is {}'.format(average))
--- Média.py
+++
@@ -1,9 +0,0 @@
-# Pedir ao utilizador para inserir duas notas
-nota1 = float(input('Escreve uma nota: '))
-nota2 = float(input('Escreve outra nota: '))
-
-# Calcular a média das duas notas
-media = (nota1 + nota2) / 2
-
-# Mostrar a média
-print('A média das duas notas é {}'.format(media))
