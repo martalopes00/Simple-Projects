e35644b9157d11f6e2815539dc6d6722755a6b96
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:04:38 +0100

    Update Aumento 15% salarial.py

--- Aumento 15% salarial.py
+++ Aumento 15% salarial.py
@@ -1,2 +1,8 @@
+# Pedir ao utilizador para inserir o salário
 salario = float(input('Qual é o seu salário?'))
-print('Com um aumento de 15% o seu salário é {}'.format(salario+(salario*0.15)))
+
+# Calcular o salário com aumento de 15%
+novo_salario = salario + (salario * 0.15)
+
+# Mostrar o resultado ao utilizador
+print('Com um aumento de 15% o seu salário é {}'.format(novo_salario))
