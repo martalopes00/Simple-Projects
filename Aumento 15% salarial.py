6e9b8e9f09180b4a28db97d010c349dbf38fea99
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:22:47 +0100

    Update and rename Aumento 15% salarial.py to Salary with 15% increase.py

--- Aumento 15% salarial.py
+++
@@ -1,8 +0,0 @@
-# Pedir ao utilizador para inserir o salário
-salario = float(input('Qual é o seu salário?'))
-
-# Calcular o salário com aumento de 15%
-novo_salario = salario + (salario * 0.15)
-
-# Mostrar o resultado ao utilizador
-print('Com um aumento de 15% o seu salário é {}'.format(novo_salario))
---
+++ Salary with 15% increase.py
@@ -0,0 +1,8 @@
+# Ask the user to enter their salary
+salary = float(input('What is your salary? '))
+
+# Calculate the salary with a 15% increase
+new_salary = salary + (salary * 0.15)
+
+# Show the result to the user
+print(f'With a 15% increase, your salary is {new_salary}')
