308f55192321abe978d878319bb6ca9a4a88fa5e
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:08:22 +0100

    Update Conversor de euro para dólar.py

--- Conversor de euro para dólar.py
+++ Conversor de euro para dólar.py
@@ -1,4 +1,4 @@
-# Pedir ao utilizador quanto dinheiro tem na carteira
+# Perguntar ao utilizador quanto dinheiro tem na carteira
 dinheiro = float(input('Quantos euros você tem na carteira? '))
 
 # Definir a cotação do dólar
@@ -6,3 +6,4 @@ dolar = 1.17
 
 # Calcular quantos dólares o utilizador pode comprar e mostrar o resultado
 print('você pode comprar {} dólares.'.format(dinheiro * dolar))
+
