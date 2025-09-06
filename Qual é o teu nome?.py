1a1a52c441309c17309aa41d8032d5a99ae4672b
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:27:42 +0100

    Update and rename Qual é o teu nome?.py to What is your name?.py

--- Qual é o teu nome?.py
+++
@@ -1,5 +0,0 @@
-# Pedir ao utilizador o seu nome
-nome = input('Qual é o teu nome?: ')
-
-# Responder usando o nome
-print('Prazer em conhecer-te,', nome + '!')
---
+++ What is your name?.py
@@ -0,0 +1,5 @@
+# Ask the user for their name
+name = input('What is your name?: ')
+
+# Respond using the name
+print('Nice to meet you,', name + '!')
