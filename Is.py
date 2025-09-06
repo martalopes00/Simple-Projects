f366c2f411174ea91c7a18b7ef01d6dbf728aad1
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:32:56 +0100

    Update and rename is.py to Is.py

---
+++ Is.py
@@ -0,0 +1,23 @@
+# Ask the user to type something
+something = input('Type something: ')
+
+# Show the primitive type of the value
+print('The primitive type of this value is', type(something))
+
+# Check if it contains only spaces
+print('Only spaces?', something.isspace())
+
+# Check if it is a number
+print('Is it a number?', something.isnumeric())
+
+# Check if it is alphabetic
+print('Is it alphabetic?', something.isalpha())
+
+# Check if it is uppercase
+print('Is it uppercase?', something.isupper())
+
+# Check if it is lowercase
+print('Is it lowercase?', something.islower())
+
+# Check if it is capitalized
+print('Is it capitalized?', something.istitle())
