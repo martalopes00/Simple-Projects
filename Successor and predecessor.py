7bb579ade9e719ac971917694830b59e366b75e1
Marta Lopes <marta.sl.lopes09@gmail.com>
2025-09-06 23:31:00 +0100

    Update and rename Sucessor e antecessor.py to Successor and predecessor.py

---
+++ Successor and predecessor.py
@@ -0,0 +1,5 @@
+# Ask the user to enter a number
+n = int(input('Enter a number: '))
+
+# Show the successor and predecessor of the number
+print('The successor of {} is {} and the predecessor is {}'.format(n, n+1, n-1))
--- Sucessor e antecessor.py
+++
@@ -1,5 +0,0 @@
-# Pedir ao utilizador para inserir um número
-n = int(input('Escreve um número: '))
-
-# Mostrar o sucessor e o antecessor do número
-print('O sucessor de {} é {} e o antecessor é {}'.format(n, n+1, n-1))
